eb7336b8899f395d40922b1f6ef82887c7d2ab55
sametxcr <samuel.munoz.m12@gmail.com>
2026-08-06 11:06:03 -0400

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,6 @@ const WHATSAPP = "56932285399";
 const INSTAGRAM = "https://www.instagram.com/balladaresmotor/";
 const YOUTUBE_ID = "0q6KurtImDI";
 
-export default function Page() {
-  const [showGaleria, setShowGaleria] = useState(false);
 
 const slides = [
   { title: ["repro stage", "1 & 2"], titleImgs: ["/hero/titles/repro_stage_transparent.png", "/hero/titles/1and2_transparent.png"], sub: "Potencia real +25% torque +30%", img: "/hero/repro.jpg", pos: "50% 50%" },
