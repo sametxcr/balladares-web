cf4bc5ec9adbf091db3b4a31b947173aaef17818
sametxcr <samuel.munoz.m12@gmail.com>
2026-08-06 11:09:50 -0400

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { Montserrat, Holtwood_One_SC } from "next/font/google";
 const montserrat = Montserrat({ subsets: ["latin"], weight: ["400","700","900"] });
 const holtwood = Holtwood_One_SC({ weight: "400", subsets: ["latin"] });
 const [showGaleria, setShowGaleria] = useState(false);
-
 const WHATSAPP = "56932285399";
 const INSTAGRAM = "https://www.instagram.com/balladaresmotor/";
 const YOUTUBE_ID = "0q6KurtImDI";
