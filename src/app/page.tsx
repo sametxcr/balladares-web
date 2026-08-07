96aa2d9435b6888cb6327d7858f29b71df00bb8e
sametxcr <samuel.munoz.m12@gmail.com>
2026-08-07 10:58:37 -0400

    vamos avanzando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,7 +91,7 @@ const waLinkRepuesto = "https://wa.me/" + WHATSAPP_REPUESTO + "?text=" + encodeU
       <a key={link.id} href={`#${link.id}`} className="relative px-4 py-2 border border-white/10 hover:border-red-600 hover:bg-red-600/10 group" style={{transform:"skewX(-12deg)"}}><span className="group-hover:text-red-500" style={{transform:"skewX(12deg)", display:"block"}}>{link.label}</span></a>
     ))}
   </div>
-  <h2 className={`${holtwood.className} text-[11px] md:text-lg font-black italic leading-none tracking-wider text-white text-right ml-auto translate-y-[5px] md:translate-y-[2px] pr-2`}>
+  <h2 className={`${holtwood.className} text-[11px] md:text-lg font-black italic leading-none tracking-wider text-white text-right ml-auto translate-y-[5px] md:translate-y-[2px] pr-0 translate-x-[8px] md:translate-x-[12px]`}>
   EL PODER DE <span className="text-red-600">UN BUEN SERVICIO</span>
 </h2>
 </nav>
