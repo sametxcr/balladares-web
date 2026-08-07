bb4fd5e3b8834a7165f532055d0cb2373db65808
sametxcr <samuel.munoz.m12@gmail.com>
2026-08-07 17:42:43 -0400

    vamos avanzando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -307,7 +307,7 @@ const waLinkRepuesto = "https://wa.me/" + WHATSAPP_REPUESTO + "?text=" + encodeU
   </div>
 </div>
 
-<RoadFighterArcade height="450px" pcHeight="650px" />
+<RoadFighterArcade />
 
   
   
