3689424a7a65879e7181c3bbca2eeaa53c3d901a
sametxcr <samuel.munoz.m12@gmail.com>
2026-08-20 06:29:49 -0400

    veamos

--- src/lib/tbk.ts
+++ src/lib/tbk.ts
@@ -1,6 +1,6 @@
 export const TBK = {
   ID: process.env.TBK_API_KEY_ID || process.env.TRANSBANK_COMMERCE_CODE || '597055555532',
-  SECRET: process.env.TBK_API_KEY_SECRET || process.env.TRANSBANK_API_KEY || '579B532A7440BB0C9079DED94D31EA1615BACEB36B38C77FB7D7179E317BD139F1',
+  SECRET: process.env.TBK_API_KEY_SECRET || process.env.TRANSBANK_API_KEY || '579B532A7440BB0C9079DED94D31EA1615BACEB36B38C77FB7D7179E317BD139F',
   URL: process.env.TBK_ENV === 'PRODUCTION' || process.env.TRANSBANK_ENV === 'PRODUCTION'
     ? 'https://webpay3g.transbank.cl/rswebpaytransaction/api/webpay/v1.2/transactions'
     : 'https://webpay3gint.transbank.cl/rswebpaytransaction/api/webpay/v1.2/transactions'
