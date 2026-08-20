1750cb5d44041e31fe030a83e116f691014b5719
sametxcr <samuel.munoz.m12@gmail.com>
2026-08-20 07:51:41 -0400

    create quemado final

--- src/app/api/debug/route.ts
+++ src/app/api/debug/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server'
 export async function GET(){
   return NextResponse.json({
     id:'597055555532',
-    secret_len: '579B532A7440BB0C9079DED94D31EA1615BACEB36B38C77FB7D7179E317BD139F'.length,
+    secret_len: '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C'.length,
     secret_start: '579B532A74',
     secret_end: '139F',
     env_secret_exists: !!process.env.TBK_API_KEY_SECRET,
