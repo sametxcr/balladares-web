cf17a6115859be8cdfee7f8fb08297b1335acd37
sametxcr <samuel.munoz.m12@gmail.com>
2026-08-20 08:01:18 -0400

    create quemado final

--- src/app/api/debug/route.ts
+++ src/app/api/debug/route.ts
@@ -6,7 +6,7 @@ export async function GET(){
     id:'597055555532',
     secret_len: '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C'.length,
     secret_start: '579B532A74',
-    secret_end: '139F',
+    secret_end: '36B1C',
     env_secret_exists: !!process.env.TBK_API_KEY_SECRET,
     env_secret_len: process.env.TBK_API_KEY_SECRET?.length || 0
   })
