3e5b9a9e417af04546a04b2ae8ffc8f45b024502
sametxcr <samuel.munoz.m12@gmail.com>
2026-09-02 16:01:04 -0400

    cambiando imagen

--- src/app/ventasticker/page.tsx
+++ src/app/ventasticker/page.tsx
@@ -45,9 +45,20 @@ export default function ventastickerPage() {
     ))}
   </div>
 
-  <button onClick={()=>setMenuOpen(!menuOpen)} className="lg:hidden w-10 h-10 bg-white text-black border-2 border-black flex items-center justify-center text-xl font-black">
-    {menuOpen ? '✕' : '☰'}
-  </button>
+  <button
+  onClick={()=>setMenuOpen(!menuOpen)}
+  className="lg:hidden absolute top-2 right-2 w-7 h-7 bg-black flex items-center justify-center z-[101] active:scale-95"
+>
+  <img
+  src={menuOpen? "/X.png" : "/casco.png"}
+  alt="menu"
+  className="w-8 h-8 object-contain"
+  style={{
+    filter: menuOpen? "none" : "brightness(0) invert(1)",
+    mixBlendMode: "screen"
+  }}
+/>
+</button>
 </nav>
 
 {menuOpen && (
